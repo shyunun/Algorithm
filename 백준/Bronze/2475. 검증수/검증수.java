a022c9e0c0a09198194cdfaf4b1fe85622d18fd4
shyunun <shyunun03@gmail.com>
2025-05-20 01:16:03 +0900

    [Bronze V] Title: 검증수, Time: 172 ms, Memory: 17564 KB -BaekjoonHub

---
+++ 백준/Bronze/2475. 검증수/검증수.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int result = 0;
+        for (int i = 0; i<5; i++) {
+            int number = sc.nextInt();
+            result += (number*number);
+        }
+        System.out.println(result%10);
+    }
+}
